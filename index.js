f5990ba11bb9fd44cc10b5c9f2a0523a69dffdbb
James Ng <njames15@gmail.com>
2023-01-16 22:47:24 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+let myBookMarks = []
+const inputEl = document.getElementById('input-el')
+const inputBtn = document.getElementById('save-btn')
+const ulEl = document.getElementById('ul-el')
+const deleteBtn = document.getElementById('delete-btn')
+const bkMarksFromLocalStorage = JSON.parse(localStorage.getItem('myBookMarks'))
+
+if (bkMarksFromLocalStorage) {
+  myBookMarks = bkMarksFromLocalStorage;
+  render(myBookMarks);
+}
+
+function render(bkmarks) {
+  let listItems = ''
+  for (let mark of bkmarks) {
+    listItems += `
+      <li>
+        <a href='${mark}' target="_blank">
+          ${mark}
+        </a>
+      </li>
+    `
+  }
+  ulEl.innerHTML = listItems
+}
+
+
+inputBtn.addEventListener("click", function() {
+  myBookMarks.push(inputEl.value);
+  inputEl.value = '';
+  localStorage.setItem("myBookMarks", JSON.stringify(myBookMarks))
+  render(myBookMarks);
+})
+
+deleteBtn.addEventListener("dblclick", function() {
+  localStorage.clear()
+  myBookMarks = []
+  render(myBookMarks);
+})
+
+
+
+
+
+
+
